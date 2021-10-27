ba2c9b27667dcda0ecd6c537f421547e2dfdf6c5
Dorian Mayamba <dorian.mayamba@gmail.com>
2021-10-27 02:03:50 +0200

    created c# project and updated readme.md

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Team_27_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
