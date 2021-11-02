dde524ea6bb68aa7580a82aef2d2fe1ae231d6c4
Dorian Mayamba <dorian.mayamba@gmail.com>
2021-11-02 13:42:18 +0100

    unity can client can now connect to the server

---
+++ GameClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Team_27_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server.start(300,8000);
+            Console.Title = "Blood lust Server";
+            Console.ReadKey();
+        }
+    }
+}
