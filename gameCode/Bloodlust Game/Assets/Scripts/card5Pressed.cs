1dc50526641731d816b2a3ace363d101ac953392
Tahseen Islam <tahseen@Tahseens-MacBook-Pro.local>
2021-11-12 21:29:18 +0000

    added event handler for when cards are pressed, and added comments to game code

---
+++ gameCode/Bloodlust Game/Assets/Scripts/card5Pressed.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class card5Pressed : MonoBehaviour
+{
+    Card card;
+    //function to select a card
+    void setCard(string select) { card = Resources.Load<Card>(select); }
+    //create string with all cards (the card the player chose is last)
+    public string createCardString()
+    {
+        StringBuilder cardData = new StringBuilder("");
+        setCard("card1");
+        cardData.Append(card.cardToString());
+        setCard("card2");
+        cardData.Append("$" + card.cardToString());
+        setCard("card3");
+        cardData.Append("$" + card.cardToString());
+        setCard("card4");
+        cardData.Append("$" + card.cardToString());
+        setCard("card5");
+        cardData.Append("$" + card.cardToString());
+        return cardData.ToString();
+    }
+    //send card data to server (for now it just prints to the console)
+    public void sendString() { Debug.Log(createCardString()); }
+}
