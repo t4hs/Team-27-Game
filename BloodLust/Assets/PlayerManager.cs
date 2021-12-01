c0ecfb4cb056e861420b39265be126ec77e8c06f
Dorian Mayamba <dorian.mayamba@gmail.com>
2021-12-01 16:41:22 +0100

    added control for received messages

--- BloodLust/Assets/PlayerManager.cs
+++ BloodLust/Assets/PlayerManager.cs
@@ -21,7 +21,11 @@ public class PlayerManager : NetworkedPlayer {
     
     // Called once a GamePlay Event is received from the server
     public override void OnMessageReceived(GamePlayEvent gamePlayEvent){
-        print ("GamePlayEvent received from server, event name: " + gamePlayEvent.eventName );    
+        //print ("GamePlayEvent received from server, event name: " + gamePlayEvent.eventName );    
+        if(gamePlayEvent.eventName.Equals("card chosen")){
+            //ToDo Game Logic
+            // Game Logic code goes here
+        }
     }
 
        
