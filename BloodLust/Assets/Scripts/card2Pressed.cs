04a66e73c820cd7536e149e366cb8b1b6da0906d
Dorian Mayamba <dorian.mayamba@gmail.com>
2021-12-01 16:29:24 +0100

    wrote the code to send the data to the server but still a draft

--- BloodLust/Assets/Scripts/card2Pressed.cs
+++ BloodLust/Assets/Scripts/card2Pressed.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using EdgeMultiplay;
 using UnityEngine;
 
-public class card2Pressed : MonoBehaviour
+public class card2Pressed : PlayerManager
 {
     Card card;
+
     //function to select a card
-    void setCard(string select) { card = Resources.Load<Card>(select); }
+    void setCard(string select)
+    {
+        card = Resources.Load<Card>(select);
+        Debug.Log (card);
+    }
+
     //create string with all cards (the card the player chooses is last)
     public string createCardString()
     {
@@ -24,6 +31,12 @@ public class card2Pressed : MonoBehaviour
         cardData.Append("$" + card.cardToString());
         return cardData.ToString();
     }
+
     //send card data to server (for now it just prints to the console)
-    public void sendString() { Debug.Log(createCardString()); }
+    public void sendString()
+    {
+        EdgeManager
+            .MessageSender
+            .BroadcastMessage("card chosen", createCardString().Split("$"));
+    }
 }
