172336dbddee85756fcde041da9700b12ef3fc83
Dorian Mayamba <dorian.mayamba@gmail.com>
2022-02-01 19:35:42 +0100

    added a feature to allow the user to join and create rooms

---
+++ BloodLust/Assets/Scripts/GameManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviourPunCallbacks
+{
+
+    [SerializeField]
+    private InputField userNameField;
+    [SerializeField]
+    private Button joinRoomButton;
+    [SerializeField]
+    private Button createRoomButton;
+    [SerializeField]
+    private InputField roomNameField;
+    [SerializeField]
+    private byte MaxPlayers = 2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        createRoomButton.onClick.AddListener(()=>{
+            this.CreateRoom(roomNameField.text);
+        });
+        joinRoomButton.onClick.AddListener(()=>{
+            this.joinRoom(roomNameField.text);
+        });
+    }
+
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined lobby");
+    }
+
+    public void CreateRoom(string roomName)
+    {
+        RoomOptions options = new RoomOptions();
+        options.IsVisible  = false;
+        options.MaxPlayers = this.MaxPlayers;
+        PhotonNetwork.CreateRoom(roomName, options, null);
+    }
+
+    public void joinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.LogErrorFormat("failed to join the room. Error dode: {0}, error mesage: {1} ", returnCode, message);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("created room");
+        SceneManager.LoadScene("RoomScene", LoadSceneMode.Single);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("joined room");
+        PhotonNetwork.LoadLevel("GameScene");
+    }
+
+}
