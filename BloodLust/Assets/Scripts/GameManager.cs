c6ffeae0e40dbd52986d6fc708dd57b19436e88d
Dorian Mayamba <dorian.mayamba@gmail.com>
2022-02-04 18:50:07 +0100

    modified the room and added the photon app_id

--- BloodLust/Assets/Scripts/GameManager.cs
+++ BloodLust/Assets/Scripts/GameManager.cs
@@ -11,71 +11,34 @@ public class GameManager : MonoBehaviourPunCallbacks
     [SerializeField]
     private InputField userNameField;
     [SerializeField]
-    private Button joinRoomButton;
+    private Button connectButton;
     [SerializeField]
-    private Button createRoomButton;
-    [SerializeField]
-    private InputField roomNameField;
-    [SerializeField]
-    private byte MaxPlayers = 2;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        PhotonNetwork.ConnectUsingSettings();
-        createRoomButton.onClick.AddListener(()=>{
-            this.CreateRoom(roomNameField.text, userNameField.text);
-        });
-        joinRoomButton.onClick.AddListener(()=>{
-            this.joinRoom(roomNameField.text);
+        connectButton.onClick.AddListener(()=>{
+            if(userNameField.text.Length >= 1)
+            {
+                PhotonNetwork.NickName = userNameField.text;
+                Debug.Log("connecting to the server");
+                PhotonNetwork.ConnectUsingSettings();
+            }
         });
     }
 
 
     public override void OnConnectedToMaster()
     {
-        Debug.Log("Connected to master");
+        Debug.Log("Connected to master. Joining a lobby");
         PhotonNetwork.JoinLobby();
     }
 
     public override void OnJoinedLobby()
     {
+        SceneManager.LoadScene("LobbyScene", LoadSceneMode.Single);
         Debug.Log("Joined lobby");
-    }
-
-    public void CreateRoom(string roomName, string playerName)
-    {
-        RoomOptions options = new RoomOptions();
-        options.IsVisible  = false;
-        options.MaxPlayers = this.MaxPlayers;
-        PhotonNetwork.NickName = playerName;
-        PhotonNetwork.CreateRoom(roomName, options, null);
-    }
-
-    public void joinRoom(string roomName)
-    {
-        PhotonNetwork.JoinRoom(roomName);
-    }
-
-    public override void OnJoinRoomFailed(short returnCode, string message)
-    {
-        Debug.LogErrorFormat("failed to join the room. Error dode: {0}, error mesage: {1} ", returnCode, message);
-    }
-
-    public override void OnCreatedRoom()
-    {
-        Debug.Log("created room");
-        SceneManager.LoadScene("RoomScene", LoadSceneMode.Single);
-    }
-
-    /**
-     * When a room has been joined
-     */
-    public override void OnJoinedRoom()
-    {
-        Debug.Log("joined room");
-        PhotonNetwork.LoadLevel("GameScene");
     }
 
 }
