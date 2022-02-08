32831de1b9dfe73f651fe3140ac00882fdbd510d
Tahseen Islam <tahseen@Tahseens-MacBook-Pro.local>
2022-02-08 14:13:32 +0000

    player class, card class and damage handler added with all methods comlete. game scene and old clutter removed.

--- BloodLust/Assets/Scripts/Card.cs
+++ BloodLust/Assets/Scripts/Card.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "new Card", menuName ="Card")]
-public class Card : ScriptableObject
+public class Card : MonoBehaviour
 {
     public string type;
-    public string damageType;
     public int damage;
-    public Sprite artwork;
 
-    //convert card data to a string
-    public string cardToString()
+    public Card()
     {
-        return type+"$"+damageType+"$"+damage.ToString();
+        generateCard();
     }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //create a card
+    void generateCard()
+    {
+        damage = Random.Range(500, 900);
+        int rndType = Random.Range(0, 4);
+        if (rndType == 0) type = "attack";
+        else if (rndType == 1) type = "counter";
+        else if (rndType == 2) type = "dodge";
+        else if (rndType == 3) type = "grapple";
+        else if (rndType == 4) type = "heal";
+    }
+
 }
---
+++ BloodLust/Assets/Scripts/damageHandler.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damageHandler : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public damageHandler()
+    {
+
+    }
+    void dealDamage(Player player, int dmg)
+    {
+        player.health -= dmg;
+    }
+    void heal(Player player, int value)
+    {
+        player.health += value;
+    }
+
+}
