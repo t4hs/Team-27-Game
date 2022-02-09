c1f61bdaa9c9a530a5559f603346989a94323740
Dorian Mayamba <dorian.mayamba@gmail.com>
2022-02-09 23:05:49 +0100

    implement events and functions to handle the character selection of each player

---
+++ BloodLust/Assets/Scripts/PlayerScript/BloodLustPlayer.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class BloodLustPlayer : MonoBehaviourPunCallbacks {
+
+    private int score = 0;
+    private Character character;
+    private bool isWinner;
+    private List<Card> hand;
+    [SerializeField] private string playerName;
+    [SerializeField] private string characterName;
+    [SerializeField] private int playerId;
+    private bool isLocal;
+
+    /*public BloodLustPlayer(string NickName, int actorNumber, bool isLocal)
+    {
+        this.playerName = NickName;
+        this.playerId = actorNumber;
+        this.isLocal = isLocal;
+        this.score = 0;
+        GenerateCards();
+    }*/
+
+    public int PlayerId
+    {
+        set{this.playerId = value; }
+        get{return this.playerId; }
+    }
+
+    public string NickName
+    {
+        set{this.playerName = value; }
+
+        get{ return this.playerName; }
+    }
+
+    public bool IsLocal
+    {
+        set{ this.isLocal = value; }
+        get{ return this.isLocal; }
+    }
+
+    public void SetCharacter(Character chosenCharacter)
+    {
+        character = chosenCharacter;
+        CharacterName = character.CharacterName;
+    }
+
+    public Character GetCharacter()
+    {
+        return character;
+    }
+
+    public string CharacterName
+    {
+        set{this.characterName = value;}
+
+        get{return this.characterName; }
+    }
+
+    public void UpdateScore(int point)
+    {
+        score+= point;
+    }
+
+    public int GetScore()
+    {
+        return this.score;
+    }
+
+    public void GenerateCards()
+    {
+        hand = new List<Card>();
+
+        while(hand.Count < 8)
+        {
+            Card card = new Card();
+            hand.Add(card);
+        }
+    }
+
+    public void SetWinner(bool winner)
+    {
+        this.isWinner = winner;
+    }
+
+    public bool IsWinner()
+    {
+        return this.isWinner;
+    }
+
+}
+
+
