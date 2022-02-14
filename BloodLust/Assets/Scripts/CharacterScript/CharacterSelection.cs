f351ce42db6f8a78ccc8dc4cc1b4900d82e1a721
Dorian-Mayamba <dorian.mayamba@gmail.com>
2022-02-14 02:51:36 +0100

    conflict resolved

--- BloodLust/Assets/Scripts/CharacterScript/CharacterSelection.cs
+++ BloodLust/Assets/Scripts/CharacterScript/CharacterSelection.cs
@@ -16,8 +16,11 @@ public class CharacterSelection : MonoBehaviourPunCallbacks
     private List<GameObject> characterInstances = new List<GameObject>();    
     private int currentCharacter;
     private BloodLustPlayer player1, player2;
-
+    [SerializeField] private Button PlayerReadyButton;
+    [SerializeField] private Button selectButton;
     public event Action<Character> OnCharacterSelected;
+    private Player player;
+    private ExitGames.Client.Photon.Hashtable customProps = new ExitGames.Client.Photon.Hashtable();
 
     public void Awake()
     {
@@ -33,99 +36,132 @@ public class CharacterSelection : MonoBehaviourPunCallbacks
 
     private void OnChosenCharacter(Character chosenCharacter)
     {
-        if(PhotonNetwork.IsConnected)
+        if(PhotonNetwork.IsMasterClient)
         {
-            if(PhotonNetwork.IsMasterClient)
-            {
-                player1.SetCharacter(chosenCharacter);
-                Debug.LogFormat("{0} chose {1}", player1.NickName, player1.GetCharacter().CharacterName);
-                }else
-                {
-                    player2.SetCharacter(chosenCharacter);
-                    Debug.LogFormat("{0} chose {1}", player2.NickName, player2.GetCharacter().CharacterName);
-                }
-            }
-
-            //ToDo event when both players have chosen a character
+            player1.ChosenCharacter = chosenCharacter;
+            player1.CharacterName = chosenCharacter.CharacterName;
+            Debug.LogFormat("{0} has selected {1}", player1.NickName, player1.CharacterName);
+            customProps["chosenCharacter"] = currentCharacter;
+            player.SetCustomProperties(customProps);
+        }else
+        {
+            player2.ChosenCharacter = chosenCharacter;
+            player2.CharacterName = chosenCharacter.CharacterName;
+            Debug.LogFormat("{0} has selected {1}", player2.NickName, player2.CharacterName);
+            customProps["chosenCharacter"] = currentCharacter;
+            player.SetCustomProperties(customProps);
         }
+    }
 
         void Start()
         {
+
+            PlayerReadyButton.gameObject.SetActive(false);
 
             if(PhotonNetwork.IsConnected)
             {
                 InstanciatePrefabs();
             }
+
+            PlayerReadyButton.onClick.AddListener(()=>{
+                OnPlayerPressed();
+                });
         }
 
-        public void InstanciatePrefabs()
+        public void OnPlayerPressed()
         {
-            foreach(KeyValuePair<int, Player> player in PhotonNetwork.CurrentRoom.Players)
+           if(PhotonNetwork.IsConnected)
+           {
+            Debug.LogFormat("heading to the chatadcter scene");
+            PhotonNetwork.LoadLevel("GameScene");
+        }
+    }
+
+    void Update()
+    {
+        if(FighterReady() && PhotonNetwork.IsMasterClient)
+        {
+            PlayerReadyButton.gameObject.SetActive(true);
+            selectButton.gameObject.SetActive(false);
+        }
+    }
+
+    public void InstanciatePrefabs()
+    {
+            player = PhotonNetwork.LocalPlayer;
+
+            if(PhotonNetwork.IsMasterClient)
             {
-                if(PhotonNetwork.IsMasterClient)
-                {
-                    player1 = player1Prefab.GetComponent<BloodLustPlayer>();
-                    player1.NickName = player.Value.NickName;
-                    player1.IsLocal = player.Value.IsLocal;
-                    player1.PlayerId = player.Value.ActorNumber;
-                    player1Prefab.GetComponent<Text>().text = player1.NickName;
-                    GameObject Go = Instantiate(player1Prefab);
-                    Go.transform.SetParent(GameObject.Find("PlayerListing").transform, false);
-                    break;
-                    }else
-                    {
-                        player2 = player2Prefab.GetComponent<BloodLustPlayer>();
-                        player2.NickName = player.Value.NickName;
-                        player2.IsLocal = player.Value.IsLocal;
-                        player2.PlayerId = player.Value.ActorNumber;
-                        player2Prefab.GetComponent<Text>().text = player2.NickName;
-                        GameObject Go = Instantiate(player2Prefab);
-                        Go.transform.SetParent(GameObject.Find("PlayerListing").transform, false);
-                        break;
-                    }
-                }
-
-                foreach (var character in characters)
-                {
-                    GameObject characterInstance = Instantiate(character.CharacterPrefab);
-
-                    characterInstance.SetActive(false);
-
-                    characterInstances.Add(characterInstance);
-                }
-
-                characterInstances[currentCharacter].SetActive(true);
-                characterName.text = characters[currentCharacter].CharacterName;
+                player1 = player1Prefab.GetComponent<BloodLustPlayer>();
+                player1.PlayerId = player.ActorNumber;
+                player1.NickName = player.NickName;
+                player1Prefab.GetComponent<Text>().text = player1.NickName;
+                GameObject go = Instantiate(player1Prefab);
+                go.transform.SetParent(playerListing.transform,false);
+            }else
+            {
+                player2 = player2Prefab.GetComponent<BloodLustPlayer>();
+                player2.PlayerId = player.ActorNumber;
+                player2.NickName = player.NickName;
+                player2Prefab.GetComponent<Text>().text = player2.NickName;
+                GameObject go = Instantiate(player2Prefab);
+                go.transform.SetParent(playerListing.transform,false);
             }
 
-            public void Next()
+            foreach (var character in characters)
             {
-                characterInstances[currentCharacter].SetActive(false);
+                GameObject characterInstance = Instantiate(character.CharacterPrefab);
 
-                currentCharacter = (currentCharacter + 1)% characterInstances.Count;
+                characterInstance.SetActive(false);
 
-                characterInstances[currentCharacter].SetActive(true);
-                characterName.text = characters[currentCharacter].CharacterName;
+                characterInstances.Add(characterInstance);
             }
 
+            characterInstances[currentCharacter].SetActive(true);
+            characterName.text = characters[currentCharacter].CharacterName;
+        }
 
-            public void Back()
+        public void Next()
+        {
+            characterInstances[currentCharacter].SetActive(false);
+
+            currentCharacter = (currentCharacter + 1)% characterInstances.Count;
+
+            characterInstances[currentCharacter].SetActive(true);
+            characterName.text = characters[currentCharacter].CharacterName;
+        }
+
+        public bool FighterReady()
+        {
+            bool fighterSelected = true;
+            foreach(Player player in PhotonNetwork.PlayerList)
             {
-                characterInstances[currentCharacter].SetActive(false);
-
-                currentCharacter--;
-                if(currentCharacter < 0)
+                if(!player.CustomProperties.ContainsKey("chosenCharacter"))
                 {
-                    currentCharacter += characterInstances.Count;
+                    fighterSelected = false;
                 }
-
-                characterInstances[currentCharacter].SetActive(true);
-                characterName.text = characters[currentCharacter].CharacterName;
             }
 
-            public void CharacterSelected()
-            {
-             OnCharacterSelected(characters[currentCharacter]);
-         }
+            return fighterSelected;
+        }
 
-     }
+        public void Back()
+        {
+            characterInstances[currentCharacter].SetActive(false);
+
+            currentCharacter--;
+            if(currentCharacter < 0)
+            {
+                currentCharacter += characterInstances.Count;
+            }
+
+            characterInstances[currentCharacter].SetActive(true);
+            characterName.text = characters[currentCharacter].CharacterName;
+        }
+
+        public void CharacterSelected()
+        {
+           OnCharacterSelected(characters[currentCharacter]);
+       }
+
+   }
