fb70e1a92b9505c5a362372943f760851b501a97
Dorian-Mayamba <dorian.mayamba@gmail.com>
2022-02-17 21:07:58 +0100

    modified gameManager

--- BloodLust/Assets/Scripts/Managers/GameManager.cs
+++ BloodLust/Assets/Scripts/Managers/GameManager.cs
@@ -9,17 +9,38 @@ public class GameManager : MonoBehaviourPunCallbacks
 {
     public static GameManager instance;
     public GameState state { get; private set; }
+    private GameObject player1Prefab, player2Prefab;
+    [SerializeField] private Character[] characters = default;
+    [SerializeField] private GameObject handGO;
 
     public void Awake()
     {
         instance = this;
         DontDestroyOnLoad(transform.gameObject);
+        player1Prefab = Resources.Load<GameObject>("player1Prefab");
+        player2Prefab = Resources.Load<GameObject>("player2Prefab");
     }
 
     public void Start()
     {
         changeState(GameState.CharacterSelection);
+        if(PhotonNetwork.IsConnected)
+        {
+            if(PhotonNetwork.IsMasterClient)
+            {
+                Player player = PhotonNetwork.LocalPlayer;
+                int currentCharacter = (int)player.CustomProperties["chosenCharacter"];
+
+                GameObject p1 = PhotonNetwork.Instantiate(player1Prefab.name, new Vector3(0,150,0), Quaternion.identity);
+            }
+            else
+            {
+                GameObject p2 = PhotonNetwork.Instantiate(player2Prefab.name, new Vector3(-80,150,0), Quaternion.identity);
+            }
+        }
     }
+
+
 
     public void changeState(GameState state)
     {
@@ -27,7 +48,7 @@ public class GameManager : MonoBehaviourPunCallbacks
         switch (state)
         {
             case GameState.CharacterSelection:
-                HandleCharacterSelection();
+                //HandleCharacterSelection();
                 break;
             case GameState.GameSetup:
                 HandleGameSetup();
