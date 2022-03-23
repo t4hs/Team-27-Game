f0f3759bf550298f583f3bc4e6426f6f45027336
Tahseen Islam <tahseen@Tahseens-MacBook-Pro.local>
2022-03-23 02:39:10 +0000

    added menu, login and registration forms, and scripts to communicate with the databases

---
+++ BloodLust/Assets/Scripts/Registeration.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class Registeration : MonoBehaviour
+{
+    public TMP_InputField nameField;
+    public TMP_InputField passwordField;
+    public TMP_InputField confirmPasswordField;
+    public Button registerButton;
+
+    public void callRegister()
+    {
+        StartCoroutine(Register());
+    }
+
+    IEnumerator Register()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("name", nameField.text);
+        form.AddField("password", passwordField.text);
+        WWW www = new WWW("riseoffighters.netlify.app/sqlconnect/register.php", form);
+        yield return www;
+        if(www.text == "0")
+        {
+            Debug.Log("User created successfully");
+        }
+        else { Debug.Log("ERROR: User creation was unsuccessful. error #" + www.text); }
+    }
+
+    public void verifyUserInput()
+    {
+        registerButton.interactable = (nameField.text.Length > 5 && passwordField.text.Length > 5 && passwordField.text == confirmPasswordField.text);
+    }
+}
