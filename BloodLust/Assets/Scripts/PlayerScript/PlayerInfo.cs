cde3bce417bb68096cdb06c55589cb2113bfdb5b
TheShadow6606 <hamammsoleman6606@gmail.com>
2022-03-31 00:20:24 +0100

    Added Icons, Card Art, Refactored Card Class
    Card Now Show Icons,
    Cards Now Are Unique To Each Character
    Card Class Was Refactored and now Uses The icon Images

--- BloodLust/Assets/Scripts/PlayerScript/PlayerInfo.cs
+++ BloodLust/Assets/Scripts/PlayerScript/PlayerInfo.cs
@@ -5,11 +5,15 @@ using UnityEngine.UIElements;
 
 public class PlayerInfo : MonoBehaviour
 {
+    [Header("Set From The Game Scene")]
     public Transform[] characterSpawns;
     public Hand hand;
     public GameObject healthBar1, healthBar2, energyBar1, energyBar2;
     public GameObject winScreen, LoseScreen;
     public GameObject cardSpawn;
+
+    [Header("Drag From Resources/Icons Folder")]
+    public List<nameIcons> typeIcons;
     
     public static PlayerInfo instance;
 
