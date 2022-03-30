c16ce5ea4b4459c90ec690639a630b27d9921319
Tahseen Islam <tahseen@Tahseens-MacBook-Pro.local>
2022-03-31 00:25:07 +0100

    fixed sound overlap

--- BloodLust/Assets/Scripts/dontDestroy.cs
+++ BloodLust/Assets/Scripts/dontDestroy.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class dontDestroy : MonoBehaviour
 {
+
     private void Awake()
     {
         DontDestroyOnLoad(transform.gameObject);
+    }
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "GameScene") Destroy(transform.gameObject);
     }
 }
